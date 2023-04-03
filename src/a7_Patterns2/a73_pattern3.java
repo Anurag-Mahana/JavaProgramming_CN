652f659cd709afa97bc69c70c220fbf9151dd5ad
Anurag Mahana <anuragmahana20@gmail.com>
2023-04-03 22:24:26 +0530

    Updation of existing files

--- src/a7_Patterns2/a73_pattern3.java
+++ src/a7_Patterns2/a73_pattern3.java
@@ -1,32 +1,44 @@
+
+
 package a7_Patterns2;
 import java.util.*;
 
-//Required code:
-//	---1---
-//	--121--
-//	-12321-
-//	1234321
-
-
-
 public class a73_pattern3 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String []args)throws Exception {
 		try(Scanner sc=new Scanner(System.in)){
-			for(int i=1;i<=5;i++) {
-				for(int space=1;space<=5-i;space++) {
+
+			//			pattern1
+			//			    *
+			//			   **
+			//			  ***
+			//			 ****
+
+			for(int i=1;i<=4;i++) {
+				for(int space=1;space<=4-i;space++) {
 					System.out.print(" ");
 				}
 				for(int j=1;j<=i;j++) {
-					System.out.print(j);
-				}
-				for(int k=1;k<=i-1;k++) {
-					System.out.print(k);
+					System.out.print("*");
 				}
 				System.out.println();
 			}
 
+			//			pattern2
+			//			****
+			//			***
+			//			**
+			//			*
+			System.out.println();
+			for(int i=1;i<=4;i++) {
+				for(int space=1;space<i;space++) {
+					System.out.print(" ");
+				}
+				for(int j=4;j>=i;j--) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
 		}
 	}
-
 }
